c3472a531ede2ee2d1ec1460f294012dc4f4aa80
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-06-11 12:29:47 -0700

    Added solution from the lecture

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const secrets = require('../config/secrets');
 const Users = require('../users/users-model.js');
+
+function generateToken(user) {
+  return jwt.sign({
+    userId: user.id,
+    userRole: 'student',
+  }, secrets.jwt, {
+    expiresIn: '1h',
+  }, 
+  )
+}
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -11,7 +23,11 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      const token = generateToken(saved);
+      res.status(201).json({
+        message: `Welcome ${saved.username}!`,
+        authToken: token
+      });
     })
     .catch(error => {
       res.status(500).json(error);
@@ -25,8 +41,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          authToken: token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
